db86d7c17415c7a51a8fa5290e84432b37cbd205
dnedi135 <themakc135@gmail.com>
2019-11-25 16:46:52 +0200

    added one more test

--- test/user.test.js
+++ test/user.test.js
@@ -3,15 +3,34 @@ var should = require('chai').should(),
     supertest = require('supertest'),
     api = supertest(global.URL);
 
-describe('User', function () {
-    it("Should return user by specified ID", function (done) {
-        api
-            .get("/users/1")
-            .set("Accept", "application/json")
-            .expect(200)
-            .end(function (err, res) {
-                expect(res.body).to.have.property("username");
-                done();
-            });
+describe("Express API", function() {
+    describe("user", function() {
+        it("Should return user by specified ID", function (done) {
+            api
+                .get("/users/1")
+                .set("Accept", "application/json")
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.have.property("username");
+                    done();
+                });
+        });
+    });
+
+    describe("message", function() {
+        it("should be created", function (done) {
+           api
+               .post("/messages")
+               .set("Accept", "application/json")
+               .send({
+                   "text": "Test message is here"
+               })
+               .expect("Content-Type", /json/)
+               .expect(200)
+               .end(function (err, res) {
+                   expect(res.body).to.be.not.null;
+                   done();
+               });
+        });
     });
 });
